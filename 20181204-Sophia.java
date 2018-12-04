1efa721a14779178ed897a35ab0102a2003fae2c
HengzhiChen <45375761+HengzhiChen@users.noreply.github.com>
2018-12-04 09:22:01 +0800

    Create 20181204-Sophia.java

---
+++ 20181204-Sophia.java
@@ -0,0 +1,22 @@
+//Pastpaper 2015
+//main idea:String,class
+public class HiddenWord{
+    String hiddenWord;
+    String sth;
+    public HiddenWord(String hiddenWord, String sth){
+        this.hiddenWord = hiddenWord;
+        this.sth = sth;
+        }
+    public String getHint( String hiddenWord, String sth){
+        for(int i=0,i<hiddenWord.length(),i++){
+            if(sth.get(i).equals(hiddenWord.get(i)){
+                b+=hiddenWord.get(i);
+            }
+            if(sth.get(i).compareTo(hiddenWord)=-1){
+                b+="*";}
+            else
+                b+="+";
+                }
+            return b;
+            }
+        
